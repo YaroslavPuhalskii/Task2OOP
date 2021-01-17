116bcc18b39212d73807c2fc02243e7ff05fe2e9
YaroslavPuhalskii <netpywka14@gmail.com>
2021-01-17 20:38:23 +0300

    Task 2(OOP)

---
+++ Task2OOP/Money.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2OOP
+{
+    class Money
+    {
+        public static decimal cash;
+        public Money(decimal cash)
+        {
+            this.Cash = cash;
+           
+            
+        }
+        public decimal Cash
+        {
+            get { return cash; }
+            set { if (value < 0.0m) { cash = 0.0m; }
+                else  cash = value;             }
+        }
+
+        public static decimal Operations(decimal l)
+        {
+            return cash += l; 
+        }
+
+        public override string ToString()
+        {
+            var p = cash.ToString("F", CultureInfo.InvariantCulture);
+            var p1=  cash.ToString("N", CultureInfo.InvariantCulture);
+            return p + " "+ p1;
+        } 
+
+    }
+}
---
+++ Task2OOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money m = new Money(5.231m);
+            Money.Operations(9.00m);
+            Console.WriteLine(m.ToString());
+            Console.ReadLine();
+        }
+    }
+}
